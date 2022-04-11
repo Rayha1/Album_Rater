448e4261412adfa1ff2f67364f2fc21c7d0a8c71
Rayha1 <103225542+Rayha1@users.noreply.github.com>
2022-04-11 13:42:11 +1200

    Add files via upload

---
+++ album_rater.py
@@ -0,0 +1,26 @@
+##
+# album_rater.py
+##
+# make a album rater program
+# an album contains a title, artist, genre and rating
+# you should allow the user to add, edit, delete and rate the album
+# when you rate an album, your program should recomend another album
+#(any album of the same genre)
+# you must make this program modular (functions)
+# you should use version control
+# you SHOULD make this program robust
+# YOU MUST HAVE FUN BUILDING THIS :)
+
+def print():
+    def print_dictionary(dictionary):
+    for id, song in dictionary.items():
+        for k, v in game.items():
+            print("ID: {} title: {}\tartist: {}\tgenre:{}\trating:{}".format(id, song["title"],
+                                                                         song["artist"], song["genre"], song["rating"])
+
+
+
+
+if __name__ == "__main__":
+
+    print_dictionary(albums) 
