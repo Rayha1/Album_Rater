a24d8228abccf9e1577f5ce2405a3e16a1b398ad
gansy952 <103225504+gansy952@users.noreply.github.com>
2022-05-03 15:02:13 +1200

    Added more commenting
    And fixed some details and docstrings.

--- album_rater.py
+++ album_rater.py
@@ -1,20 +1,14 @@
 ##
 # album_rater.py
+# Album rater program that allows user to add, edit, delete, rate albums
 # made with collaboration with Rayha :))
+# 03/05/22 
 
-# make a album rater program
-# an album contains a title, artist, genre and rating
-# you should allow the user to add, edit, delete and rate the album
-# when you rate an album, your program should recomend another album
-#(any album of the same genre)
-# you must make this program modular (functions)
-# you should use version control
-# you SHOULD make this program robust
-# YOU MUST HAVE FUN BUILDING THIS :)
 
-def print_dictionary(dictionary):
+def print_albums(dictionary):
     """
-    Prints the album details in the dictionary tidy
+    Takes in the dictionary
+    Prints the album details nicely in the dictionary 
     """
     for id, song in albums.items():
         print("ID:{}\ttitle:{}\tartist:{}\tgenre:{}\trating:{}"
@@ -23,7 +17,8 @@ def print_dictionary(dictionary):
 
 def add():
     """
-    User adds new album details into the dictionary using update 
+    Takes user input album details as variables
+    Updates into the dictionary 
     """
     title = input("please enter the album name: ")
     artist = input("please enter the name of the artist: ")
@@ -35,6 +30,7 @@ def add():
 
 def remove(index):
     """
+    Takes in the id index as the key
     Deletes the key of the dictionary 
     """
     del albums[index] 
@@ -49,29 +45,36 @@ def edit():
 
 def rate(index):
     """
-    Updates rating dictionary with user's input 
+    Takes in index of the selected album
+    Updates the rating that album in the dictionary 
     """
     albums[index]["rating"] = input('Please rate the album:')
     
 def index():
     """
-    Gets index of album selected by user and returns it 
+    Gets user input index of album selected 
+    Returns it 
     """
     index = int(input("Enter id of album: "))
+    
     return index
 
 def suggest():
     """
     Suggests another album based on the highest rating album's genre 
     """
+    
     pass
     
 def menu():
     """
     Loops menu 
     """
+    
     menu = True
     while menu:
+
+        # Prints out menu and asks for user input
         option = input("\nAlbum rater"
                        "\n1) Add an album"
                        "\n2) Edit an album"
@@ -81,24 +84,42 @@ def menu():
                        "\n6) Print albums"
                        "\n0) QUIT"
                        "\nEnter a option: ")
+
         if option == '1':
+            # User adds a album
             add()
+
         elif option == '2':
+            # User edits a album 
             edit()
+            
         elif option == '3':
+            # Takes in index of a album and removes it
             remove(index())
+            
         elif option == '4':
+            # Takes in index of a album and user rates it
             rate(index())
+            
         elif option == '5':
+            # Suggests a album(havn't done yet)
             suggest()
+            
         elif option == '6':
-            print_dictionary(albums)
+            # Prints out album details nicely 
+            print_albums(albums)
+            
         elif option == '0':
+            # Breaks loop/menu
             menu = False
+            
         else:
+            # Invalid input 
             print("\nInvalid option")
 
+# Main Routine
 if __name__ == "__main__":
+    
     # Album Dictionary 
     albums = {1:{"title":"Album1", "artist": "Artist1", "genre": "pop", "rating":""},
               2:{"title":"Album2", "artist": "Artist2", "genre": "pop", "rating":""},
@@ -106,7 +127,9 @@ if __name__ == "__main__":
               4:{"title":"Album4", "artist": "Artist4", "genre": "rock", "rating":""},
               5:{"title":"Album5", "artist": "Artist5", "genre": "jazz", "rating":""},
               6:{"title":"Album6", "artist": "Artist6", "genre": "jazz", "rating":""}}
-    print_dictionary(albums)
+
+    # Prints album details nicely 
+    print_albums(albums)
 
     # Calls menu 
     menu()
